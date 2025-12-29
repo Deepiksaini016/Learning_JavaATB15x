8501ac9a1284235a77d4fde4b152939037e585a7
Deepikasaini016 <deepikasaini016@gmail.com>
2025-12-29 15:30:01 +0530

    Please add these files

---
+++ src/ex_02_Java_Basics/Lab012_Common_Math_Exception_L.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab012_Common_Math_Exception_L {
+    public static void main(String[] args) {
+        //System.out.println('10/0'); not supported in Java
+        //System.out.println(10/0);  // by zero
+        //System.out.println(0/100); // accepted
+        //System.out.println(0/0); //
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab014_Main_Method.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basics;
+
+public class Lab014_Main_Method {
+//    public static void main(String[] args) {
+//
+//        }
+//
+//    public static void main(String[] args) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+      public static void Main(String[] args) {
+
+}
+// JVM only recognise "public static void main(String[]" this part
+//
+// We can have multiple main function in a program but only 1 will run
+
+}
+
+}
+
